44138fcd82b33437b04652eb428ef2f82ef9f635
Dmytro Turskyi <dmytro.turskyi@gmail.com>
2025-05-22 20:41:54 -0400

    :art: Add rel="noopener noreferrer" to external links in Footer for improved security

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -20,12 +20,26 @@ export default function Footer() {
           href="https://play.google.com/store/apps/details?id=com.turskyi.lifecoach"
           target="_blank"
           style={{ marginLeft: 10 }}
+          rel="noopener noreferrer"
         >
           <Image
             src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"
             alt="Get it on Google Play"
             width={240}
             height={80}
+          />
+        </a>
+        <a
+          href="https://apps.apple.com/ca/app/life-coaching-ai/id6743688857"
+          target="_blank"
+          style={{ marginLeft: 10 }}
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="https://developer.apple.com/assets/elements/badges/download-on-the-app-store.svg"
+            alt="Download on the App Store"
+            width={200}
+            height={67}
           />
         </a>
       </div>
